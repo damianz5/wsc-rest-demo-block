1ab7afbda9cad5edce8f54dcb80b058691a20408
Web Summer Camp 2016 <websc@websummercamp.com>
2016-08-31 15:47:58 +0200

    a-part-2

--- DependencyInjection/EzSystemsRESTDemoBlockExtension.php
+++ DependencyInjection/EzSystemsRESTDemoBlockExtension.php
@@ -27,7 +27,6 @@ class EzSystemsRESTDemoBlockExtension extends Extension implements PrependExtens
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        $loader->load('config.yml');
     }
 
     /**
